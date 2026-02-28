8a8cc4d73e2563f2eb9a028db113702d873cbad2
xiaoshuangLi <qq1157502759@gmail.com>
2026-03-01 00:02:05 +0800

    backup

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,6 @@
 import { createProgram } from '@/cli/program';
 
-// 如果直接运行此文件，则启动 CLI 程序
-if (require.main === module) {
-  createProgram().parseAsync(process.argv).catch((err) => {
-    console.error(err);
-    process.exit(1);
-  });
-}
+createProgram().parseAsync(process.argv).catch((err) => {
+  console.error(err);
+  process.exit(1);
+});
